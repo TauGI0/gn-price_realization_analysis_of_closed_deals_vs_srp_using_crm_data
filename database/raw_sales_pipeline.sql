138fba556164887efe48ef6e4d16609cd40aeffd
TauG10 <noga.gio.02@gmail.com>
2026-02-12 20:37:48 +0800

    Added DDL and DML scripts for schema creation and data loading

---
+++ database/raw_sales_pipeline.sql
@@ -0,0 +1,10 @@
+CREATE TABLE raw_sales_pipeline (
+    opportunity_id TEXT,
+    sales_agent TEXT,
+    product TEXT,
+    account TEXT,
+    deal_stage TEXT,
+    engage_date DATE,
+    close_date DATE,
+    close_value NUMERIC(18,2)
+)
---
+++ database/stg_sales_pipeline.sql
@@ -0,0 +1,13 @@
+CREATE TABLE stg_sales_pipeline (
+    opportunity_id TEXT,
+    sales_agent TEXT,
+    product TEXT,
+    account TEXT,
+    deal_outcome TEXT,
+    engage_date DATE,
+    close_date DATE,
+    deal_duration INTEGER,
+    deal_duration_bucket_month TEXT,
+    close_value NUMERIC(18,2),
+    price_adjustment_pct NUMERIC(6,5)
+);
