125bf74c8ffd31ffe44d2fcea7fee3a67f6f3146
TauG10 <noga.gio.02@gmail.com>
2026-03-31 20:45:26 +0800

    feat: Add query and results for KPI no.3 and 4. Remove resources for win rate KPI

--- dataset_analysis/queries/KPI_03_win_rate.sql
+++
@@ -1,9 +0,0 @@
--- Win Rate Calculation
--- Win Rate = (Number of Won Deals) / (Total Number of Deals)
-
-SELECT (CAST(COUNT(*) AS DECIMAL) /
-	(SELECT COUNT (*)
-		FROM fact_sales_pipeline)) AS win_rate
-FROM fact_sales_pipeline
-WHERE deal_outcome = 'Won';
-
